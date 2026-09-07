61d8e08fc441e02808785430178c6540f6a46109
essi4 <essi7688@gmail.com>
2026-09-07 22:36:46 +0330

    add FOT10 installable app manifest

---
+++ app/manifest.js
@@ -0,0 +1,15 @@
+export default function manifest() {
+  return {
+    name: "FOT10 | Football Live",
+    short_name: "FOT10",
+    description: "نتایج زنده، لیگ‌ها، تیم‌ها و آمار فوتبال",
+    start_url: "/",
+    display: "standalone",
+    background_color: "#071018",
+    theme_color: "#071018",
+    orientation: "portrait",
+    lang: "fa",
+    dir: "rtl",
+    icons: [],
+  };
+}
