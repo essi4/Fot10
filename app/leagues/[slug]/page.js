afa269a1b5b888b5b2bf6c028d4da233265930f9
essi4 <essi7688@gmail.com>
2026-09-10 23:08:14 +0330

    feat(leagues): add mobile league hub shell

--- app/leagues/[slug]/page.js
+++ app/leagues/[slug]/page.js
@@ -1,7 +1,12 @@
 import League360Page from "./League360Page";
+import styles from "./league.module.css";
 
 export const dynamic = "force-dynamic";
 
 export default function LeagueDetailPage({ params, searchParams }) {
-  return <League360Page params={params} searchParams={searchParams} />;
+  return (
+    <div className={styles.page}>
+      <League360Page params={params} searchParams={searchParams} />
+    </div>
+  );
 }
