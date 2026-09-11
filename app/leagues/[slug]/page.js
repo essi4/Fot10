bf75fb093eb0d5b87f3f0c89fb8b747f16fb1fe4
essi4 <essi7688@gmail.com>
2026-09-11 12:32:33 +0330

    feat(leagues): switch competition route to unified hub

--- app/leagues/[slug]/page.js
+++ app/leagues/[slug]/page.js
@@ -1,4 +1,4 @@
-import League360Page from "./League360Page";
+import League360PageV2 from "./League360PageV2";
 import styles from "./league.module.css";
 
 export const dynamic = "force-dynamic";
@@ -6,7 +6,7 @@ export const dynamic = "force-dynamic";
 export default function LeagueDetailPage({ params, searchParams }) {
   return (
     <div className={styles.page}>
-      <League360Page params={params} searchParams={searchParams} />
+      <League360PageV2 params={params} searchParams={searchParams} />
     </div>
   );
 }
